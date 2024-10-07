b8b42ef179cb668f34bc3e8cfa61b60a106c3a78
Philippe Maincon <philippe.maincon@sintef.no>
2024-10-07 10:51:15 +0200

    GLMakieTester is mixin on GLMakie, not overloading: GLMakie not a dependency of "test"

--- test/GLMakieTester.jl
+++ test/GLMakieTester.jl
@@ -1,8 +1,7 @@
-using GLMakie
 struct SpyAxe
     call::Vector{Any}
 end
 SpyAxe() = SpyAxe(Any[])
-GLMakie.lines!(axe::SpyAxe,args...;kwargs...)   = push!(axe.call,(fun=:lines!,args=args,kwargs=kwargs))
-GLMakie.scatter!(axe::SpyAxe,args...;kwargs...) = push!(axe.call,(fun=:scatter!,args=args,kwargs=kwargs))
+lines!(axe::SpyAxe,args...;kwargs...)   = push!(axe.call,(fun=:lines!,args=args,kwargs=kwargs))
+scatter!(axe::SpyAxe,args...;kwargs...) = push!(axe.call,(fun=:scatter!,args=args,kwargs=kwargs))
 
