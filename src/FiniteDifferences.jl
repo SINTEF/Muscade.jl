d1320e430f5491b5701b73be3ccdfa562cc74d5f
Philippe Maincon <philippe.maincon@sintef.no>
2024-10-09 13:28:16 +0200

    reverted to FiniteDifferences.jl as were before merge

--- src/FiniteDifferences.jl
+++ src/FiniteDifferences.jl
@@ -1,17 +1,12 @@
-
-
 # table...[order+1][left or center or right][point in kernel]
-const table_of_finite_diff_kernels            = [ [[(Δs=0,w=1.)]],
-                                                  [[(Δs=0,w=-1/2),(Δs=2,w=1/2)], [(Δs=-2,w=-1/2),(Δs=0,w=1/2)], [(Δs=-1,w=-1/2),(Δs=1,w=1/2)] ],
+const table_of_finite_diff_kernels            = [ [[(Δs=0,w=1.)] ],
+                                                  [[(Δs=0,w=-1.),(Δs=1,w=1.)], [(Δs=-1,w=-1.),(Δs=0,w=1.)], [(Δs=-1,w=-1/2),(Δs=1,w=1/2)] ],
                                                   [[(Δs=0,w=1.),(Δs=1,w=-2.),(Δs=2,w=1.)], [(Δs=-2,w=1.),(Δs=-1,w=-2.),(Δs=0,w=1.)], [(Δs=-1,w=1.),(Δs=0,w=-2.),(Δs=1,w=1.)]]  ]
 
 const table_of_finite_diff_kernels_transposed = [ [ [(Δs=0,w=1.)] ],
-                                                  [[(Δs=0,w=-.5),(Δs=1,w=-.5)], [(Δs=1,w=-.5)], [(Δs=-2,w=.5),(Δs=-1,w=.5),(Δs= 1,w=-.5)],
-                                                  [(Δs= 2,w=-.5),(Δs=1,w=-.5),(Δs=-1,w=.5)], [(Δs=-1,w=.5)],[(Δs=0,w=.5),(Δs=-1,w=.5)],
-                                                   [(Δs= -1,w=.5),(Δs= 1,w=-.5)]],
-                                                  [[(Δs=0,w=1.),(Δs=1,w=1.)], [(Δs=-1,w=-2.),(Δs=0,w=-2.),(Δs= 1,w=1.)], [(Δs=-2,w=1.),(Δs=-1,w=1.),(Δs=0,w=-2.),(Δs= 1,w=1.)], 
-                                                   [(Δs= 2,w=1.),(Δs= 1,w=1.),(Δs=0,w=-2.),(Δs=-1,w=1.)],[(Δs= 1,w=-2.),(Δs=0,w=-2.),(Δs=-1,w=1.)], [(Δs=0,w=1.),(Δs=-1,w=1.)], 
-                                                   [(Δs=-1,w=1.),(Δs=0,w=-2.),(Δs=1,w=1.)]                                            ] ] 
+                                                  [[(Δs=0,w=-1.),(Δs=1,w=-1/2)],[(Δs=-1,w=1.),(Δs=1,w=-1/2)],[(Δs=-1,w=1/2),(Δs=1,w=-1.)],[(Δs=-1,w=1/2),(Δs=0,w=1.)],[(Δs=-1,w=1/2),(Δs=1,w=-1/2)]],
+                                                  [[(Δs=0,w=1.),(Δs=1,w=1.)], [(Δs=-1,w=-2.),(Δs=0,w=-2.),(Δs= 1,w=1.)], [(Δs=-2,w=1.),(Δs=-1,w=1.),(Δs=0,w=-2.),(Δs= 1,w=1.)], [(Δs= 2,w=1.),(Δs= 1,w=1.),(Δs=0,w=-2.),(Δs=-1,w=1.)],
+                                                   [(Δs= 1,w=-2.),(Δs=0,w=-2.),(Δs=-1,w=1.)], [(Δs=0,w=1.),(Δs=-1,w=1.)], [(Δs=-1,w=1.),(Δs=0,w=-2.),(Δs=1,w=1.)]                                            ] ] 
 # npoints_transposed[order] NB: this is for a linear combination of derivatives up to `order`
 
 
@@ -40,26 +35,39 @@ function finitediff(order,n,s;transposed=false)
     q = table_of_finite_diff_kernels_transposed
     if transposed
         if     order == 0 q[1][1]
-        else
-            if     s==1   q[order+1][1]
-            elseif s==2   q[order+1][2]   
-            elseif s==3   q[order+1][3]
-            elseif s==n-2 q[order+1][4]
-            elseif s==n-1 q[order+1][5]
-            elseif s==n   q[order+1][6]
-            else          q[order+1][7]
+        elseif order == 1
+            if     s==1   q[2][1] 
+            elseif s==2   q[2][2] 
+            elseif s==n-1 q[2][3]
+            elseif s==n   q[2][4]
+            else          q[2][5] 
+            end
+        elseif order == 2 
+            if     s==1   q[3][1]
+            elseif s==2   q[3][2]   
+            elseif s==3   q[3][3]
+            elseif s==n-2 q[3][4]
+            elseif s==n-1 q[3][5]
+            elseif s==n   q[3][6]
+            else          q[3][7]
             end
         end        
     else
         if     order == 0 p[1][1]
-        else
-            if     s==1   p[order+1][1]   
-            elseif s==n   p[order+1][2] 
-            else          p[order+1][3]  
+        elseif order == 1
+            if     s==1   p[2][1]
+            elseif s==n   p[2][2] 
+            else          p[2][3]
+            end
+        elseif order == 2
+            if     s==1   p[3][1]   
+            elseif s==n   p[3][2] 
+            else          p[3][3]  
             end
         end
     end
 end
+
 
 
 
