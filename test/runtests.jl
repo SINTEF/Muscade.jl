9f9fcfdc8b70c425e2203f3cd48b60980c4c62e5
Thomas Sauder <Thomas.Sauder@sintef.no>
2024-12-02 08:50:45 +0100

    Using Pkg in runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 test    = @__DIR__
 muscade = normpath(joinpath(test,".."))
 docs    = normpath(joinpath(test,"../docs"))
+using Pkg
 Pkg.activate(test)
 using Muscade # seems necessary for doc test to work on a cold start
 module Runtest
