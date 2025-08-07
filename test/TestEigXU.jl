5a0c3ad1a63f5497c25ca6afc237a4148b57b87a
Philippe Maincon <philippe.maincon@sintef.no>
2025-08-07 11:25:50 +0200

    fixed "tuple" which should have been "ntuple"...

--- test/TestEigXU.jl
+++ test/TestEigXU.jl
@@ -51,12 +51,6 @@ nakedmesh   = mesh[inaked,:]
 strainmesh  = mesh[istrain,:]
 accmesh     = reshape(Xnod[iacc],(length(iacc),1))
 addelement!(model,EulerBeam3D{hasU},nakedmesh;mat=mat,orient2=SVector(0.,0.,1.))
-
-
-# StrainGaugeOnEulerBeam3D(::Teleobj, ::Treq, ::SMatrix{3, 1, Float64, 3},   ::SMatrix{3, 1, Float64, 3},   ::SVector{1, Float64},      ::SVector{1, Float64},      ::SVector{1, Float64},      ::SVector{1, Float64},      ::SVector{1, Float64})
-# StrainGaugeOnEulerBeam3D(::Teleobj, ::Treq, ::SMatrix{3, Ngauge, Float64}, ::SMatrix{3, Ngauge, Float64}, ::SVector{Ngauge, Float64}, ::SVector{Ngauge, Float64}, ::SVector{Ngauge, Float64}, ::SVector{Ngauge, Float64}, ::Float64) where {Ngauge, Teleobj, Treq}
-
-
 addelement!(model,ElementCost,strainmesh;
                         req           = @request(ε),
                         cost          = (eleres,X,U,A,t) -> sum((eleres.ε/σε).^2)/2,
@@ -66,22 +60,16 @@ addelement!(model,ElementCost,strainmesh;
                                          ElementType   = EulerBeam3D{true},
                                          elementkwargs = (mat     = mat,
                                                           orient2 = SVector(0.,0.,1.))))
-
 addelement!(model,ElementCost,accmesh;
                         req           = @request(a),
                         cost          = (eleres,X,U,A,t) -> sum((eleres.a/σa).^2)/2,
                         ElementType   = Position3D,
                         elementkwargs = (P             = SMatrix{3,3}(0.,0.,.1,  0.,0.,.1,  0.,0.,.1),
                                          D             = SMatrix{3,3}(1.,0.,0.,  0.,1.,0.,    0.,0.,1.) ))
-
-
-
-
 # Ucost
 addelement!( model, SingleDofCost, Muscade.columnmatrix(Unod         )    ,class=:U, field=:t1,cost=QuadraticFunction(0.,0.5(σu^-2)))
 addelement!( model, SingleDofCost, Muscade.columnmatrix(Unod         )    ,class=:U, field=:t2,cost=QuadraticFunction(0.,0.5(σu^-2)))
 addelement!( model, SingleDofCost, Muscade.columnmatrix(Unod         )    ,class=:U, field=:t3,cost=QuadraticFunction(0.,0.5(σu^-2)))
-
 # disp meas
 addelement!( model, SingleDofCost, Muscade.columnmatrix(Xnod[istrain])    ,class=:X, field=:t1,cost=QuadraticFunction(0.,0.5(σx^-2)))
 addelement!( model, SingleDofCost, Muscade.columnmatrix(Xnod[istrain])    ,class=:X, field=:t2,cost=QuadraticFunction(0.,0.5(σx^-2)))
@@ -89,7 +77,7 @@ addelement!( model, SingleDofCost, Muscade.columnmatrix(Xnod[istrain])    ,class
 
 initialstate      = initialize!(model)   
 initialstate.time     = 0.
-OX,OU                 = 2,2
+OX,OU                 = 2,0
 
 ## N
 σₓᵤ     = (X=(t1 =xn,t2 =xn,t3 =xn,r1 =xn,r2 =xn,r3 =xn,
@@ -105,7 +93,7 @@ OX,OU                 = 2,2
 
 ## EigXU analysis
 Δω                = 2^-6 
-p                 = 5#11
+p                 = 11
 nmod              = 5
 
 
@@ -124,38 +112,38 @@ nmod              = 5
 
 
 
-# eigincXU          = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=true,verbosity=1,tol=1e-20,σₓᵤ)
+eigincXU          = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=true,verbosity=1,tol=1e-20,σₓᵤ)
 
-# nα                = 32
-# α                 = 2π*(1:nα)/nα
-# circle            = 0.05*[cos.(α) sin.(α)]'
-# GUI(initialstate,eigincXU;shadow = (;EulerBeam3D              = (;style=:shape,line_color=:grey,Udof=false),
-#                                     StrainGaugeOnEulerBeam3D  = (;gauge_color=:transparent)         ),
-#                           model  = (;EulerBeam3D              = (;style=:solid,section=circle),
-#                                      StrainGaugeOnEulerBeam3D = (;L=0.03),
-#                                      Position3D               = (;L=.03)) ) 
+nα                = 32
+α                 = 2π*(1:nα)/nα
+circle            = 0.05*[cos.(α) sin.(α)]'
+GUI(initialstate,eigincXU;shadow = (;EulerBeam3D              = (;style=:shape,line_color=:grey,Udof=false),
+                                    StrainGaugeOnEulerBeam3D  = (;gauge_color=:transparent)         ),
+                          model  = (;EulerBeam3D              = (;style=:solid,section=circle),
+                                     StrainGaugeOnEulerBeam3D = (;L=0.03),
+                                     Position3D               = (;L=.03)) ) 
 
 
-using SnoopCompileCore, SnoopCompile, AbstractTrees, ProfileView
-using Profile
-using BenchmarkTools
+# using SnoopCompileCore, SnoopCompile, AbstractTrees, ProfileView
+# using Profile
+# using BenchmarkTools
 
-mission = :profile
-if mission == :report
-    out = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=true,verbosity=1,tol=1e-20,σₓᵤ)
-elseif mission == :time
-    out = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=true,verbosity=1,tol=1e-20,σₓᵤ)
-    @btime out =solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=false,verbosity=1,tol=1e-20,σₓᵤ)
-elseif mission == :profile
-    out = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=true,verbosity=1,tol=1e-20,σₓᵤ)
-    Profile.clear()
-    Profile.@profile for i=1:30
-        local out = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=false,verbosity=1,tol=1e-20,σₓᵤ)
-    end
-    ProfileView.view(fontsize=30);
-    # After clicking on a bar in the flame diagram, you can type warntype_last() and see the result of 
-    # code_warntype for the call represented by that bar.
-end
+# mission = :profile
+# if mission == :report
+#     out = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=true,verbosity=1,tol=1e-20,σₓᵤ)
+# elseif mission == :time
+#     out = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=true,verbosity=1,tol=1e-20,σₓᵤ)
+#     @btime out =solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=false,verbosity=1,tol=1e-20,σₓᵤ)
+# elseif mission == :profile
+#     out = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=true,verbosity=1,tol=1e-20,σₓᵤ)
+#     Profile.clear()
+#     Profile.@profile for i=1:30
+#         local out = solve(EigXU{OX,OU};Δω, p, nmod,initialstate,verbose=false,verbosity=1,tol=1e-20,σₓᵤ)
+#     end
+#     ProfileView.view(fontsize=30);
+#     # After clicking on a bar in the flame diagram, you can type warntype_last() and see the result of 
+#     # code_warntype for the call represented by that bar.
+# end
 ;
 
 
