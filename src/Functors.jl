8bf08c75d970dab12117255558e17d6d2e6b83d5
Philippe Maincon <philippe.maincon@sintef.no>
2025-09-02 14:54:40 +0200

    added functor code - still have to create tests

--- src/Functors.jl
+++ src/Functors.jl
@@ -1,6 +1,3 @@
-
-# TODO: FunctionWrappers.jl
-
 """
     f  = FunctionFromVector(xs::AbstractRange,ys::AbstractVector)
     y  = f(x)
@@ -21,34 +18,7 @@ function (f::FunctionFromVector)(x)
     return f.y[i]*(1-di)+f.y[i+1]*di
 end
 
-struct QuadraticFunctionWithConstantMean <: Function
-    μ::𝕣
-    σ::𝕣
-end
-struct QuadraticFunctionWithMeanFuncOfTime{F} <:Function
-    μ::F
-    σ::𝕣
-end
-"""
-    f  = QuadraticFunction(μ,σ)
 
-`μ` and `σ` are `𝕣` (`Float64`)
-
-    y  = f(x) # == 1/2*((x-μ)/σ)^2
-    f  = QuadraticFunction(μ,σ)
-
-Alternatively, `μ` can be a `Function` of time, in which case
-
-    y  = f(x,t) # == 1/2*((x-μ(t))/σ)^2
-
-"""
-QuadraticFunction(μ::𝕣       ,σ::𝕣) = QuadraticFunctionWithConstantMean(  μ,σ)
-QuadraticFunction(μ::Function,σ::𝕣) = QuadraticFunctionWithMeanFuncOfTime(μ,σ)
-
-(f::QuadraticFunctionWithConstantMean  )(x,args...) = .5*((x-f.μ   )/f.σ)^2  # args... allows to ignore an extra t argument
-(f::QuadraticFunctionWithMeanFuncOfTime)(x,t) = .5*((x-f.μ(t))/f.σ)^2
-
-export Functor
 struct Functor{name,Ta} 
     captured::Ta
     function Functor{name}(;kwargs...) where{name}
@@ -56,19 +26,62 @@ struct Functor{name,Ta}
         return new{name,typeof(nt)}(nt)
     end
 end
+"""
+    a = 3
+    @functor (a,e=2) function f(x::Real)
+        return a*x^e
+    end
 
-# search "Functor" in BasicElements.jl, and StaticXUA001.jl
+or
 
-# planned syntactic sugar:
-#    @capture(a,e=2) f(x::Float64)=a*x^e 
-# generates
-    # f = Functor{:f}(;a,e=2)  
-    # (o::Functor{:f})(x::Float64) = o.captured.a*x^o.captured.e
-# while
-#    @functormethod f(x::Int)=a*x+e 
-# generates
-    # (o::Functor{:f})(x::Int) = o.captured.a*x+o.captured.e
-# and
-#    @functor(a,e=2) f 
-# generates
-    # f = Functor{:f}(;a,e=2)
+    a = 3
+    @functor (a,e=2)  f(x::Real)=a*x^e
+
+Defines `f::Functor{:f}`, which can be passed as an argument to some constructors.
+This has advantages over a closure defined as
+
+    f(x::Real)=a*x^e
+
+- A closure captures a variable "by reference", while `@functor` captures it by value, which
+might be more intuitive. 
+- To ensure type stability, the variables captured by a closure
+would have to be declared `const`.
+- If the code of the function is not changed, the function is not parsed and compiled again.
+
+It is not possible to associate multiple methods to a functor.
+
+"""
+macro functor(capturedargs,foo)
+    @assert capturedargs.head == :tuple
+    caparglist         = capturedargs.args
+    ncaparg            = length(caparglist)
+    capargnames        = Vector{Symbol}(undef,ncaparg)
+    for iarg           = 1:ncaparg 
+        arg            = caparglist[iarg]
+        capargnames[iarg] = arg isa Symbol ? arg : arg.args[1]
+    end
+
+    @assert foo.head == :(=) || foo.head == :function
+    functionheader     = foo.args[1]
+    functionname       = functionheader.args[1]
+    functionarglist    = functionheader.args[2]
+    functionsym        = QuoteNode(functionname)
+    functionbody       = foo.args[2]
+    functionbody       = MacroTools.postwalk(functionbody) do ex
+        ex isa Symbol && ex∈capargnames ? :(o.captured.$ex) : ex
+    end
+    ex                 = MacroTools.postwalk(rmlines,foo)
+    ex                 = MacroTools.postwalk(unblock,ex)
+    qex                = QuoteNode(ex) # unannotated code for the function 
+    tag                = Symbol("tag_for_the_functor_macro_",functionname)
+    return prettify(esc(quote
+        $functionname = Functor{$functionsym}(;$(capturedargs)...)            # f = Functor{:f}(;(a,e=2)...)  
+        if  ~@isdefined($tag) || $tag≠$qex
+            $tag = $qex
+            function (o::Functor{$functionsym})($functionarglist)     # function (o::Functor{:f})(x::Float64)
+                $functionbody                                         #     o.captured.a*x^o.captured.e 
+            end                                                       # end
+        end                                                          
+    end))
+end
+
