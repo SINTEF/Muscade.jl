67040f64dc6d689d4464467b2baf0a20b3c51295
Thomas Sauder <thomas.sauder@sintef.no>
2026-05-20 21:45:37 +0200

    reverting temporarily to rectangular cross section, adding TuningFork.jl  to the example suite

--- examples/TuningFork.jl
+++ examples/TuningFork.jl
@@ -5,18 +5,19 @@
 
 using Muscade, StaticArrays, GLMakie, Muscade.Toolbox
 
-# Parameters of the problem, dimensions of the tuning fork, number of elements
+# Dimensions of the tuning fork and number of elements
 l₀=2.5e-2;  n₀=1    # base - length and number of elements 
 l₁=1e-2;    n₁=1    # fork - length and number of elements per side
 l₂=9.5e-2;  n₂=5    # prong - length and number of elements per side
-b = 5e-3;           # square cross-section, length of the square size
-E = 210e9;          # Young modulus for steel
-G = 79.3e9;         # shear modulus for steel
-μ = 7850. *b^2;     # mass per unit length
-beamCrossSection = BeamCrossSection(EA=E*b^2, EI₂=E*b^4/12, EI₃=E*b^4/12, GJ=G*b^4/3, μ=μ, ι₁=μ/6*b^2,Cl₂=1.,Cl₃=1.); 
+h = 5e-3;           # width of the tuning fork cross-section
+b = 5e-3;           # thickness of the cross-section
+E = 210e9;          # Young modulus
+G = 79.3e9;         # shear modulus
+μ = 7850. *h*b;     # mass per unit length, assumes steel
+beamCrossSection = BeamCrossSection(EA=E*h*b, EI₂=E*h^3*b/12, EI₃=E*b*h^3/12, GJ=G*h*b^3/3, μ=μ, ι₁=μ/12*(h^2+b^2),Cl₂=1.,Cl₃=1.); 
 
 # Approximation of the fork fundamental frequency (cantilever beam bending)
-f₁ = 1.875^2/(2*π*l₂^2)*√(E*b^4/12/μ); 
+f₁ = 1.875^2/(2*π*l₂^2)*√(E*h^3*b/12/μ) 
 
 # Define an A-parametrized point mass element (inducing an inertia force)
 struct LumpedMass <: AbstractElement; m :: 𝕣; end
@@ -42,7 +43,7 @@ XnodeCoord = vcat(
 pling = Muscade.FunctionFromVector([-10, 0, 5/f₁, (5+1/2)/f₁, 10],[0,0,10,0,0]); @functor with() pling_(t) = pling(t)
 plingNode = n₀+2n₁+n₂+1; # where do we hit the tuning fork
 
-# # Start building the model
+# # Building the model
 model       = Model(:TuningFork);
 
 # Add the nodes describing the beam model, the node that will contain the parameter to optimize, and  the node that will contain the load to estimate in DirectXUA
